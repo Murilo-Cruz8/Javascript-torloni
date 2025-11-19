5a1700971acb680cfb4bedf6f956e5ef02ccf56a
MURILO FRANCINELLI CRUZ <44970126895@imaccommand028.local>
2025-11-19 10:29:05 -0300

    exemplo js

---
+++ Aula01.js
@@ -0,0 +1,66 @@
+// window.alert("Essa frase e minha")
+
+// window.confirm("Não é minha")
+
+
+// window.prompt("qual é o seu nome?")
+
+// let nome = window.prompt("Qual é o seu nome")
+
+// window.alert("Bem vindo!" + nome)
+
+
+// let numero01 = parseInt(prompt("Digite o primeiro número: "))
+
+
+// let numero02 = parseInt(prompt("digite o segundo número: "))
+
+// let soma = numero01 + numero02
+
+// console.log(soma)
+
+// let num01 = parseFloat(prompt("digite algum número: "))
+
+// let num02 = parseFloat(prompt("digite algum número: "))
+
+// let resultado = num01 + num02
+
+// console.log(resultado)
+
+// let idade = parseInt(prompt("Qual é sua idade?"))
+
+// if (idade >= 18){
+//     console.log("você é maior de idade, da-lhee")
+// }
+// else{
+//     console.log("Você é menor de idade, kinho")
+// }
+
+let estacoes = prompt("digite uma estações do ano: ")
+
+switch (estacoes) {
+    case "Verão":
+        console.log("Muito calor e praia")
+
+        break;
+
+    case "outono":
+        console.log("As folhas caem no final")
+
+        break;
+
+    case "iverno":
+        console.log("Está muito frio")
+
+        break;
+
+    case "Primavera":
+        console.log("Eu vejo flores em você kinho e jetulinho.")
+
+        break;
+
+    default:
+
+        break;
+
+}
